afadbb10b094e143191084f68da83c2436a3eb72
Ryan McLean <pvtryan100@gmail.com>
2018-04-18 21:42:41 +0100

    PlexConnect Initial Checkin

---
+++ plexconnect/run.sh
@@ -0,0 +1,15 @@
+#!/bin/ash
+
+PCDIR=/srv/plexconnect
+CERTS=assets/certificates
+PEM=${PCDIR}/${CERTS}/trailers.pem
+CER=${PCDIR}/${CERTS}/trailers.cer
+KEY=/tmp/trailers.key
+
+# Create Certs
+if [ ! -f ${PEM} ] || [ ${CREATE_CERT} ];  then
+  openssl req -new -nodes -newkey rsa:2048 -out ${PEM} -keyout ${KEY} -x509 -days 7300 -subj "/C=US/CN=trailers.apple.com"
+  openssl x509 -in ${PEM} -outform der -out ${CER} && cat ${KEY} >> ${PEM}
+fi
+
+/usr/bin/python ${PCDIR}/PlexConnect.py
